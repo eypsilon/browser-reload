105707a8ef1ff9ae93325f2a14383d9ba789dd70
Engin Ypsilon <engin@ok.de>
2022-05-02 22:44:53 +0200

    Script corrections

--- browser-reload.php
+++ browser-reload.php
@@ -9,7 +9,7 @@
  * @var array Config */
 $config = \array_merge([
     'output'       => true,                                    // [false | 'minimal' | true]
-    'set_timeout'  => 0.2,                                     // > 0 (pause between reloads, if multiple windows)
+    'set_timeout'  => 0.1,                                     // > 0 (pause between reloads, if multiple windows)
     'trigger_key'  => 'ctrl+r',                                // ['F5' | 'ctrl+r']
     'srch_title'   => 'local-dev-many-title',                  // identifier
     'srch_browser' => ['Navigator', 'Google-chrome', 'Opera'], // Navigator = Firefox
@@ -22,7 +22,7 @@ $config = \array_merge([
  * @author Engin Ypsilon <engin.ypsilon@gmail.com>
  * @license http://opensource.org/licenses/mit-license.html MIT License
  */
-class XdoTool
+class BrowserReload
 {
 
     /**
@@ -57,7 +57,7 @@ class XdoTool
     private function windows(array $arr, array $r=[]): array
     {
         foreach($arr as $b)
-            $r[] = $this->xtool("search --classname {$b}");
+            $r[] = $this->xtool("search --onlyvisible --classname {$b}");
         return \explode("\n", \trim(\implode("\n", $r)));
     }
 
@@ -78,10 +78,10 @@ class XdoTool
             $backTo = $this->xtool('getwindowfocus');
             foreach($searchWindows as $id) {
                 $wn = $this->xtool("getwindowname $id");
-                if ($wn AND \str_contains($wn, $conf['srch_title'])) {
+                if (\str_contains($wn, $conf['srch_title'])) {
                     $this->xtool("windowactivate {$id}; sleep {$conf['set_timeout']}");
                     $this->xtool("key --clearmodifiers --window {$id} {$conf['trigger_key']}");
-                    $r['windows'][$id] = \str_replace(" {$conf['srch_title']}", '', $wn);
+                    $r['reload'][$id] = \str_replace(" {$conf['srch_title']}", '', $wn);
                 }
             }
             // reactivate where we started 9 lines ago
@@ -89,7 +89,7 @@ class XdoTool
 
             $r['count'] = [
                 'windows' => \count($searchWindows),
-                'reload'  => \count($r['windows'] ?? [])
+                'reload'  => \count($r['reload'] ?? [])
             ];
         }
         return $r;
@@ -97,18 +97,19 @@ class XdoTool
 
 }
 
+
 /**
  * @var array run */
-$run = (new XdoTool)->run($config);
+$run = (new BrowserReload)->run($config);
 
 if ('false' === $config['output'])
     exit;
 
 exit(\json_encode('minimal' === $config['output']
-    ? ($run['windows'] ?? $run)
+    ? ($run['reload'] ?? $run)
     : [
         'argv'   => $argv,
         'config' => $config,
-        'reload' => $run,
+        'window' => $run,
     ], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)
 );
